b9037572d7cd29a7ecec1e7828c8dbf09570f36b
Etki <etki@etki.me>
2017-11-13 22:08:31 +0300

    Added processor output recognition

--- src/API/Conversion/ProcessorInterface.php
+++ src/API/Conversion/ProcessorInterface.php
@@ -8,13 +8,16 @@ use AmaTeam\Image\Projection\API\Tile\TileInterface;
 /**
  * Interface for tile post-processor that allows to further twiddle tile
  * (e.g. apply FXAA).
+ *
+ * Processor may operate on existing tile or return another one as a
+ * replacement.
  */
 interface ProcessorInterface
 {
     /**
      * @param TileInterface $tile
      * @param SpecificationInterface $specification
-     * @return void
+     * @return TileInterface|null
      */
     public function process(
         TileInterface $tile,
